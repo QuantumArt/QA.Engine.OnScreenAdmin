44c814c513aab191d4f2d0c65997dfb0391cf46a
MikulenyaI <mikulenyai@quantumart.ru>
2020-03-25 17:07:10 +0300

    Добавил возможно выбора элемента

--- QA.Engine.OnScreenAdmin.Web/ClientApp/containers/componentsOutlines.js
+++ QA.Engine.OnScreenAdmin.Web/ClientApp/containers/componentsOutlines.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
-import { getComponentsListSelector } from 'selectors/componentTree';
+import { getComponentsListSelector, getShowOnlyWidgetsSelector } from 'selectors/componentTree';
 import ComponentsOutlines from 'Components/ComponentsOutlines';
 import { updateComponents } from 'actions/componentTreeActions';
 
 
 const mapStateToProps = state => ({
   components: getComponentsListSelector(state),
+  showOnlyWidgets: getShowOnlyWidgetsSelector(state),
 });
 
 const mapDispatchToProps = dispatch => ({
