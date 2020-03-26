f3c2ea175c780b73c65a2599b4f974726dad27dd
MikulenyaI <mikulenyai@quantumart.ru>
2020-03-26 16:45:33 +0300

    Добавил кнопку для редактирования

--- QA.Engine.OnScreenAdmin.Web/ClientApp/Components/ComponentsOutlines/ComponentOutline.jsx
+++ QA.Engine.OnScreenAdmin.Web/ClientApp/Components/ComponentsOutlines/ComponentOutline.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import { Settings } from 'material-ui-icons';
 
 const styles = () => ({
   highlightsItem: {
@@ -11,6 +12,21 @@ const styles = () => ({
     '&:hover': {
       borderStyle: 'dashed',
     },
+  },
+  highlightsBody: {
+    'height': '100%',
+    'width': '100%',
+    'position': 'relative',
+    'opacity': '0',
+    'transition': 'opacity 0.3s',
+    '&:hover': {
+      opacity: '1',
+    },
+  },
+  editButton: {
+    top: 0,
+    right: 0,
+    position: 'absolute',
   },
 });
 
@@ -37,7 +53,24 @@ function ComponentOutline({ showAllZones, showAllWidgets, coords, component, cla
       onClick={() => console.log(`Добавить обработку выбора элемента: ${component.type} ${component.onScreenId}`)}
       className={`${classes.highlightsItem} component--${component.onScreenId}`}
       style={inlineStyles}
-    />
+    >
+      {component.type === 'widget' && (
+        <div className={classes.highlightsBody}>
+          <i
+            tabIndex={0}
+            role="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log(`Добавить обработку редактирования ${component.type} ${component.onScreenId}`);
+            }}
+            className={classes.editButton}
+          >
+            <Settings style={{ fontSize: 14 }} />
+          </i>
+        </div>
+      )
+      }
+    </div >
   );
 }
 
