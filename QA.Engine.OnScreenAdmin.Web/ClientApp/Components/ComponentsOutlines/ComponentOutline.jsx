9d11bbd9fd84b1bf7f4c5392d575ebc7844c3aba
MikulenyaI <mikulenyai@quantumart.ru>
2020-03-30 15:15:25 +0300

    #149559 Поправил 5 и 6 баги

--- QA.Engine.OnScreenAdmin.Web/ClientApp/Components/ComponentsOutlines/ComponentOutline.jsx
+++ QA.Engine.OnScreenAdmin.Web/ClientApp/Components/ComponentsOutlines/ComponentOutline.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+
 import { Settings } from 'material-ui-icons';
 
 const styles = () => ({
@@ -23,6 +24,13 @@ const styles = () => ({
       opacity: '1',
     },
   },
+  componentName: {
+    top: 0,
+    right: '22px',
+    position: 'absolute',
+    fontSize: '14px',
+    fontStyle: 'italic',
+  },
   editButton: {
     top: 0,
     right: 0,
@@ -32,12 +40,21 @@ const styles = () => ({
 });
 
 function ComponentOutline({ showAllZones, showAllWidgets, coords, component, classes, onSelectComponent }) {
+  let color = null;
+  if (component.type === 'zone') {
+    color = '#66bb6a';
+  } else if (component.type === 'widget') {
+    color = '#29b6f6';
+  } else if (component.type === 'article') {
+    color = '#ff9900';
+  }
+
   let inlineStyles = {
     top: `${coords.top}px`,
     left: `${coords.left}px`,
     width: `${coords.width}px`,
     height: `${coords.height}px`,
-    borderColor: component.type === 'widget' ? '#29b6f6' : '#66bb6a',
+    borderColor: color,
   };
 
   if (component.isSelected || (showAllZones && component.type === 'zone') || (showAllWidgets && component.type === 'widget')) {
@@ -57,6 +74,7 @@ function ComponentOutline({ showAllZones, showAllWidgets, coords, component, cla
     >
       {component.type === 'widget' && (
         <div className={classes.highlightsBody}>
+          <span className={classes.componentName}>{component.properties.title}</span>
           <i
             tabIndex={0}
             role="button"
@@ -66,11 +84,15 @@ function ComponentOutline({ showAllZones, showAllWidgets, coords, component, cla
             }}
             className={classes.editButton}
           >
-            <Settings style={{ fontSize: 14 }} />
+            <Settings style={{ fontSize: 20 }} />
           </i>
         </div>
-      )
-      }
+      )}
+      {(component.type === 'zone') && (
+        <div className={classes.highlightsBody}>
+          <span className={classes.componentName}>{component.properties.zoneName}</span>
+        </div>
+      )}
     </div >
   );
 }
