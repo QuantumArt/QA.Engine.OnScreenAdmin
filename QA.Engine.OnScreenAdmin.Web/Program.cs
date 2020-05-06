f92fa32439630e0cbb28f82b9d38ddf9a58ced73
Pavel Celut <CelutP@gmail.com>
2020-05-06 17:51:26 +0300

    Switch to structure logging

--- QA.Engine.OnScreenAdmin.Web/Program.cs
+++ QA.Engine.OnScreenAdmin.Web/Program.cs
@@ -11,6 +11,7 @@ namespace QA.DotNetCore.OnScreenAdmin.Web
     {
         public static void Main(string[] args)
         {
+            NLog.LogManager.LoadConfiguration("nlog.config");
             BuildWebHost(args).Run();
         }
 
